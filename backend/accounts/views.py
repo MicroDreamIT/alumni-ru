69725563143245ff9794073a0f62f915596a3252
sharif <shahanur700@gmail.com>
2023-06-07 01:24:09 +0600

    adding accounts

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from pytz import unicode
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class LoginView(APIView):
+    authentication_classes = TokenAuthentication,
+    permission_classes = IsAuthenticated,
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = authenticate(
+                request,
+                username=serializer.validated_data['username'],
+                password=serializer.validated_data['password']
+            )
+            if user is not None:
+                login(request, user)
+                return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def get(self, request):
+        content = {
+            'user': {
+                'username': request.user.username,
+                'email': request.user.email,
+                'first_name': request.user.first_name,
+                'last_name': request.user.last_name
+            },  # `django.contrib.auth.User` instance.
+            'auth': unicode(request.auth),  # None
+        }
+        return Response(content)
